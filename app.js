e7efab4a4c2fa48611a74f02d2eb9d6fc05b7144
Juan Daniel Castaño <juancastano0398@gmail.com>
2022-10-31 20:44:22 +0100

    sad

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+const express = require('express')
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+const app = express()
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+app.use(express.json())
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+app.get('/home', ( req,res ) => {
+    res.status(200).json({ message : 'Vamos bien amazon' })
+})
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
+const PORT = 4040
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
-});
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+app.listen( PORT, () => {
+    `Escuchando por el puerto: ${PORT}`
+} )
