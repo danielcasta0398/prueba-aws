72a501adfd37ae8fbfd7bdf35d75f7d376fd2286
Juan Daniel Castaño <juancastano0398@gmail.com>
2022-10-31 15:57:26 +0100

    p

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.use( express.json() )
 
+app.get('/', ( req,res ) => { res.status(200).json({ message: "Excelente desde amazon" }) })
+
 const PORT = 4040
 
 app.listen( PORT, () => {
